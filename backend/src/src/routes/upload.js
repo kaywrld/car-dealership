dd4637c1a72117029e28b6c6501434393962b3c9
Kaywrld <kaywrld3002@gmail.com>
2026-04-19 18:26:46 +0200

    Deleted src

--- backend/src/src/routes/upload.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router  = express.Router()
-const protect = require('../middleware/protect')
-const { uploadImage, deleteImage } = require('../controllers/uploadController')
-
-router.post('/',    protect, uploadImage)
-router.delete('/',  protect, deleteImage)
-
-module.exports = router
